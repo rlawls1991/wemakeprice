21ba719ca3c0661746fb18c09a13160e5fc59163
rlawls1991 <rlawls1991@gmail.com>
2021-04-01 02:22:02 +0900

    기본세팅 완료

---
+++ wemakeprice/src/main/java/com/wemakeprice/WemakepriceApplication.java
@@ -0,0 +1,13 @@
+package com.wemakeprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemakepriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemakepriceApplication.class, args);
+	}
+
+}
---
+++ wemakeprice/src/test/java/com/wemakeprice/WemakepriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemakeprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WemakepriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
