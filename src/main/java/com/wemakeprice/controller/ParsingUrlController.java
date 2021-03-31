1ed8d479964a5f6abd7f1e5f81debc7238425f8a
rlawls1991 <rlawls1991@gmail.com>
2021-04-01 07:26:01 +0900

    feat - controller 생성

---
+++ src/main/java/com/wemakeprice/controller/ParsingUrlController.java
@@ -0,0 +1,17 @@
+package com.wemakeprice.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ParsingUrlController {
+
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+}
